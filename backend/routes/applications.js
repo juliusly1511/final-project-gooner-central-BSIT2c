4f0974c02f41b047d38892358456d263804d6be6
juliusly1511 <jbnavarro1515@gmail.com>
2026-05-01 16:02:47 +0800

    moved models to database

--- backend/routes/applications.js
+++ backend/routes/applications.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const Job = require('../models/Job');
-const Application = require('../models/Application');
+const Job = require('../../database/models/Job');
+const Application = require('../../database/models/Application');
 const { requireRole } = require('../middleware/auth');
 
 const uploadDir = path.join(__dirname, '..', 'public', 'uploads');
